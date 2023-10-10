3f4207f4a2d4b46dc3bc2ec07e7e2f26bef2a3e3
kiyomototakuto <kiyomototakuto@gmail.com>
2023-10-10 18:09:26 +0100

    Fix Sample_DRS4.py

--- Script/Sample_DRS4.py
+++ Script/Sample_DRS4.py
@@ -5,7 +5,7 @@ Treelist = ["TreeAfterPulseSeq1_0", "TreeAfterPulseSeq2_0"]
 path = "/Users/kiyomoto/reaserch/231010/"
 
 for item in Treelist:
-    DRS.Creat_ROOT_file(original_file, item, path + original_file.split(".root")[0] + "_" +item + ".root", item, "wform1", "wform0", 1400)
+    DRS.Creat_ROOT_file(original_file, item, path + item + ".root", item, "wform1", "wform0", 1400)
 
 DRS.Merge_ROOT_file(Treelist[0], Treelist[1], original_file.split(".root")[0] + "_sabun" + ".root")
 
